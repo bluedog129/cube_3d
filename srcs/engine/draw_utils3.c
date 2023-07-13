5fb5f3235d53aae3a15d9287d46c434ab3ae22fc
YongHyun-Yi <dldydgus1995@naver.com>
2023-07-13 20:25:50 +0900

    미니맵 시도중...

--- srcs/engine/draw_utils3.c
+++ srcs/engine/draw_utils3.c
@@ -1,6 +1,38 @@
 
 #include "cub3d.h"
 
+void	darwing_wall(t_game_data *game_data, t_img_data *screen)
+{
+	// 맵 배열을 벗어나면 안됨
+	// 미니맵 사이즈를 벗어나면 안됨
+	// 기준점은 플레이어의 좌표 및 미니맵 화면 중앙
+	int	y;
+	int	x;
+
+	y = 0;
+	while (y < 100)
+	{
+		x = 0;
+		while (x < 100)
+		{
+			if (game_data->camera.pos.x - (x / 15) >= 0 && game_data->camera.pos.x + (x / 15) <= game_data->map_info->width)
+			{
+				if (game_data->camera.pos.y - (y / 15) >= 0 && game_data->camera.pos.y + (y / 15) <= game_data->map_info->height)
+				{
+					if (game_data->map_info->map_board[y / 15][x / 15] == '1')
+					{
+						pixel_put_to_image(screen, x, y, create_trgb(0, 0, 255, 0));
+					}
+				}
+			}
+			x++;
+		}
+		y++;
+	}
+
+	pixel_put_to_image(screen, 50, 50, create_trgb(0, 255, 0, 0));
+}
+
 void	filling_background(t_img_data *screen)
 {
 	int	x;
@@ -32,6 +64,8 @@ void	drawing_minimap(t_game_data *game_data)
 	&screen.img_line_len, &screen.img_endian);
 
 	filling_background(&screen);
+
+	darwing_wall(game_data, &screen);
 
 	mlx_put_image_to_window(game_data->mlx_ptr, game_data->win_ptr, screen.img_ptr, 530, 10);
 
